192dd251226487ab06f866ba20664170da83e794
carlost1504 <carlost1504@gmail.com>
2023-04-10 17:24:11 -0500

    create login

---
+++ tasks/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+
+# Create your views here.
+def home(request):
+    #title='hello word'
+    #return HttpResponse(title)
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method=='GET':
+        return render(request,'singup.html',{
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                #regirter user
+                user= User.objects.create_user(username=request.POST['username'],
+                password=request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('tasks')
+            except:
+                return render(request,'singup.html',{
+                    'form': UserCreationForm,
+                    'error':'Username alredy exists'
+                })
+                 
+        return render(request,'singup.html',{
+            'form': UserCreationForm,
+            'error':'Username alredy exists'
+        })
+
+
+    return render(request,'singup.html',{
+        'form': UserCreationForm,
+        'error':'Password do not match'
+    })
+
+def tasks(request):
+    return render(request,'tasks.html')
+
+def signout (request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    return render(request, 'signin.html')
